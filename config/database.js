7d10a379e5165619190fa207395de372812814e9
keithjhinesjr <KeithHinesJr@Keiths-MacBook-Pro.local>
2016-11-17 23:31:20 -0500

    fixed DB url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://todo01:todoapp1@ds151917.mlab.com:51917/code_lou_project', //path for using remote URL
-    localUrl: 'mongodb://todoappdb' // path for local database
+    localUrl: 'mongodb://localhost/todoappdb' // path for local database
 };
